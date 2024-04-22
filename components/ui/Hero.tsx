97ba10fdc4a72a5aebf42015cb59de5d7bf80154
unknown <greeshmavg77@gmail.com>
2024-04-22 16:54:50 +0530

    nav

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,21 +1,51 @@
+import Link from "next/link";
 import React from "react";
-function Hero() {
+import Image from "next/image";
+import { NAV_LINKS } from "@/constant";
+const Navbar = () => {
   return (
-    <div className="hero">
-      <div className="padding-x lg:px-10 px-5">
-        <div className="lg:grid grid-row-2 gap-4">
-          <div className="padding-x lg:px-20 lg:pt-10">
-            <h1 className="hero__title mb-1">HOME</h1>
-            <h1 className="hero__title mb-1">SERVICE</h1>
-            <h1 className="hero__title mb-1">SOLUTIONS</h1>
-            <h1 className="hero__title mb-1">ABOUT US</h1>
-            <h1 className="hero__title mb-1">CONTCT US</h1>
-          </div>
+    <header className="w-full absolute z-10  bg-gray-800">
+      <nav className=" max-w-[1440px] h-[80px] mx-auto flex justify-between items-center sm:px-16 px-6 py-5">
+        <Link href="/" className="flex justify-center items-center gap-3">
+          <image></image>
+          <p className="text-white font-bold text-[18px] tracking-[-1] leading-5">
+            eminencetel
+          </p>
+        </Link>
+        <div className="flex gap-[70px] ">
+        {NAV_LINKS.map((link) => (
+          <Link
+            href={link.href}
+            key={link.key}
+            className="nav-link flex 
+            text-white font-bold  group/nav cursor-pointer flex- transition-all h-[50px] items-center hover:border-b-[3px] border-sky-500"
+          >
+            {link.label}
+            {link.icon_s === "true" && (
+              <span>
+                <svg
+                  aria-hidden="true"
+                  width="6px"
+                  height="6px"
+                  fill="none"
+                  font-semibold
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m19 9-7 7-7-7"
+                  />
+                </svg>
+              </span>
+            )}
+          </Link>
+        ))}
         </div>
-      </div>
-    </div>
-    
+      </nav>
+    </header>
   );
-}
+};
 
-export default Hero;
+export default Navbar;
---
+++ constant/index.ts
@@ -0,0 +1,7 @@
+// NAVIGATION BAR
+export const NAV_LINKS = [
+  { href: "/", key: "HOME", label: "HOME", icon_s: "true" },
+  { href: "/", key: "ABOUT", label: "ABOUT", icon_s: "true" },
+  { href: "/", key: "SERVICE", label: "SERVICE", icon_s: "true" },
+  { href: "/", key: "CONTACT ", label: "CONTACT ", icon_s: "true" },
+];
